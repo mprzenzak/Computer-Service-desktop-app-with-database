a598111df3eae33a4921a07c5d96b39a677e4137
Mikołaj Przenzak <mprzenzak@it-dev.pl>
2023-01-19 17:03:38 +0100

    Assembly info restored

--- Computer Service/Computer Service/Properties/AssemblyInfo.cs
+++ Computer Service/Computer Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("computer service")]
+[assembly: AssemblyTitle("Computer Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("computer service")]
+[assembly: AssemblyProduct("Computer Service")]
 [assembly: AssemblyCopyright("Copyright © HP 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
